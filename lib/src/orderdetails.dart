91f562d8b6ec3a824b96df65b100b251681fe4de
Namakula Josephine <jnamakula324@gmai.com>
2023-08-18 19:01:55 +0300

    commented out unused imports

--- lib/src/orderdetails.dart
+++ lib/src/orderdetails.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:flutter/material.dart';
+//import 'package:lottie/lottie.dart';
 import '../utils/app_styles.dart';
 import 'package:intl/intl.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 
 class OrderDetails extends StatefulWidget {
   final String userId;
